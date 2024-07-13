18300ad8dde7839845dbd0b2975e70a05e2d8473
umair <umairid112233@gmail.com>
2024-07-13 16:37:51 +0500

    create ui of case management

--- DMS-Frontend/src/app/demo/components/cases/case-management/case-management.component.ts
+++ DMS-Frontend/src/app/demo/components/cases/case-management/case-management.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CaseInfo, CaseType } from 'src/app/models/case.model';
 
 @Component({
   selector: 'app-case-management',
@@ -6,5 +7,58 @@ import { Component } from '@angular/core';
   styleUrl: './case-management.component.scss'
 })
 export class CaseManagementComponent {
+  items = [
+    { label: 'Case Type' },
+    { label: 'Case Info' },
+    { label: 'Case Review' }
+  ];
+  caseInfo: CaseInfo = new CaseInfo();
+
+  caseTypes: CaseType[] = [
+    { name: 'Surgical Guide', code: 'surgicalguide' },
+    { name: 'Crowns', code: 'crowns' },
+    { name: 'Implant Crowns', code: 'implantcrowns' }
+  ];
+
+
+  activeIndex: number = 0;
+  selectedCaseType: CaseType = null;
+
+
+  constructor() {
+    this.caseInfo = new CaseInfo();
+  }
+
+  isCaseTypeValid(): boolean {
+    return this.selectedCaseType !== null;
+  }
+
+  toggleFastDelivery() {
+    this.caseInfo.isFastDelivery = !this.caseInfo.isFastDelivery;
+  }
+
+  onFileSelect(event: any) {
+    const files = event.files;
+    this.caseInfo.scans = [];
+
+    this.caseInfo.scans.push(files);
+    console.log(this.caseInfo.scans, "this.caseInfo.scans")
+  }
+
+  prev() {
+    this.activeIndex--;
+  }
+
+  next() {
+    if (this.activeIndex < 2) {
+      this.activeIndex++;
+    } else {
+      this.submit();
+    }
+  }
+
+  submit() {
+    console.log(this.caseInfo);
+  }
 
 }
