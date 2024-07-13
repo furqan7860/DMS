667e0e10ef56675210ba7f80b3f7d1097fc4fc76
Furqan <furqanamjad60@gmail.com>
2024-07-13 17:10:58 +0500

    Revert "add frontend and backend"
    This reverts commit 30b008eaf8b176841517701761602d8694a11b0b.

--- ng/src/app/api/models/todo.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export interface Todo {
-  desc?: string;
-  id?: number;
-  isComplete?: boolean;
-  title: string;
-}
--- ng/src/app/api/services.ts
+++
@@ -1,4 +0,0 @@
-export { PatientHistoryControllerService } from './services/patient-history-controller.service';
-export { PatientControllerControllerService } from './services/patient-controller-controller.service';
-export { UserControllerService } from './services/user-controller.service';
-export { TodoControllerService } from './services/todo-controller.service';
